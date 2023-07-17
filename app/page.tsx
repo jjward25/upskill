c9fb90876250e1fc313d8af104ab3e1afa9de81e
jjward25 <45015765+jjward25@users.noreply.github.com>
2023-07-17 08:00:46 -0400

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,145 @@
-import Image from 'next/image'
-import Other from "../public/other.json";
+"use client"
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Typography from './typography';
+import content from '../employees.json';
 
-export default function Home() {
+
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+ const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+export default function employeeCards() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+
   return (
-    <main>
-      <div className='text-4xl'>People:</div>
-      <div className='flex-wrap justify-start px-20 py-5 h-full md:flex' >
-          {Other.map((card) => {
-            return (
-              <div key={card.index} className='grid-cols-1  border-2 border-emerald-950 py-3 px-6 rounded-2xl my-5 mx-auto md:w-[280px] md:h-[420px]'> 
-                <a
-                  href={card.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  alt={card.title}
-                  title={card.title}
-                  className='relative max-w-full and h-auto my-0 mx-auto border-1'
+    <main className='grid grid-cols-[20%_80%] min-h-screen w-full max-w-full'>
+      <div className='flex flex-col min-h-screen bg-white rounded-r-xl p-5'>
+        <h3 className='h-min font-bold mb-5'>My Team</h3>
+        <h3 className='h-min font-bold mb-5'>Search Keywords:</h3>
+        <p>Use this searchbar to search for keywords to identify potential project team members with relevant experience to the new project you're staffing, or to see potential internal candidates for an opening.</p>
+        </div> 
+      <div className='flex flex-wrap h-min p-5 min-w-fit'>
+
+      <div className='flex flew-wrap h-min font-bold mb-5'>{content.accountExecutives &&
+        content.accountExecutives.map((item, i) => (
+            <Card sx={{ Width: 345, Height:420 }} key={i} className='rounded-2xl mx-5 my-5 w-345'>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    R
+                  </Avatar>
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={item.firstLast}
+                subheader= {item.role}
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                image={item.imgLink}
+                alt="Paella dish"
+                className='h-17'
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  Company Start Date: {item.companyStartDate}
+                  <br/>Role Start Date: {item.roleStartDate}
+                  <br/>Role Tenure Rank: {item.roleTenureRank}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+              
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
                 >
-                  <Image src={card.source} alt={card.title} className='rounded-2xl md:height=440 md:width=240' height="440" width="240" />
-                </a>
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph className='font-bold text-lg'>Current Role:</Typography>
+                  <Typography paragraph className='font-bold'>Key Metrics:</Typography>
+                  <Typography paragraph>
+                    Customer Satisfaction: {item.roleDevelopment.rolePerformance.customerSatisfaction}
+                    <br/>Signed Deals: {item.roleDevelopment.rolePerformance.signedDeals}
+                    <br/>Gross Renewal Rate: {item.roleDevelopment.rolePerformance.grossRenewalARR}
+                    <br/>Net Renewal Rate: {item.roleDevelopment.rolePerformance.netRenewalARR}
+                    <br/>Avg Lifetime Value: {item.roleDevelopment.rolePerformance.avgCustomerLifetimeValue}
+                  </Typography>
+                  <Typography paragraph className='font-bold'>Certifications + Enablement:</Typography>
+                  <Typography paragraph>
+                     {item.roleDevelopment.certificationsEnablement}
+                  </Typography>
+                  <Typography paragraph className='font-bold'>Workflow Analytics:</Typography>
+                  <Typography paragraph>
+                     Average Book Size L12 Mos: {item.roleDevelopment.workflowAnalytics.avgBookSize}
+                     <br/>Avg Monthly Client Time: {item.roleDevelopment.workflowAnalytics.avgClientTime}Hrs
+                  </Typography>
+                  <Typography paragraph className='font-bold'>Other:</Typography>
+                  <Typography paragraph>
+                    Note: {item.roleDevelopment.Other.Note}
+                  </Typography>
 
-                <p>{card.title}</p> //other card title
+                  <Typography paragraph className='font-bold text-lg'>Career:</Typography>
+                  <Typography paragraph className='font-bold'>Common Next Roles:</Typography>
+                  <Typography paragraph>
+                    {item.careerDevelopment.commonNextRoles}
+                  </Typography>
+                  <Typography paragraph className='font-bold'>Related Certfications + Enablement:</Typography>
+                  <Typography paragraph>
+                    {item.careerDevelopment.relatedCertificationsEnablement}
+                  </Typography>
+                  <Typography paragraph className='font-bold'>Prior Experience:</Typography>
+                  <Typography paragraph>
+                    SkillX: {item.careerDevelopment.priorExperience.Skill}
+                  </Typography>
+                  
+                </CardContent>
+              </Collapse>
+            </Card>
+        ))
+      }
+     </div>
 
-                <p>
-                  {card.description} //other card description
-                </p>
-
-                <div>  //other tags wrap
-                  {card.tags.map((tag) => {
-                    return (
-                      <p key={tag.index}>  //other skill tag
-                        {tag}
-                      </p>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+     </div>
     </main>
-  )
+);
 }
